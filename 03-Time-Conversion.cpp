f6d7d6adb2db6da4eea2303ced9be3622a7c8f4a
Pallavi P N <pallavipn622@gmail.com>
2026-09-23 18:31:52 +0530

    Create 03-Time-Conversion.cpp

---
+++ 03-Time-Conversion.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string timeConversion(string s) {
+    int hour = stoi(s.substr(0, 2));
+    string period = s.substr(8, 2);
+
+    if (period == "AM") {
+        if (hour == 12)
+            hour = 0;
+    } 
+    else {
+        if (hour != 12)
+            hour += 12;
+    }
+
+    stringstream ss;
+    ss << setw(2) << setfill('0') << hour;
+
+    return ss.str() + s.substr(2, 6);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
