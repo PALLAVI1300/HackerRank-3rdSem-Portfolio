54daf3738612ebff542cf6cef4165d84d33a56a0
Pallavi P N <pallavipn622@gmail.com>
2026-09-23 18:33:43 +0530

    Implement Sparse Arrays matchingStrings function

---
+++ 05-Sparse-Arrays.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+vector<int> matchingStrings(vector<string> stringList, vector<string> queries) {
+    unordered_map<string, int> frequency;
+
+    for (const string& s : stringList) {
+        frequency[s]++;
+    }
+
+    vector<int> result;
+
+    for (const string& q : queries) {
+        result.push_back(frequency[q]);
+    }
+
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string stringList_count_temp;
+    getline(cin, stringList_count_temp);
+
+    int stringList_count = stoi(ltrim(rtrim(stringList_count_temp)));
+
+    vector<string> stringList(stringList_count);
+
+    for (int i = 0; i < stringList_count; i++) {
+        string stringListItem;
+        getline(cin, stringListItem);
+
+        stringList[i] = stringListItem;
+    }
+
+    string queries_count_temp;
+    getline(cin, queries_count_temp);
+
+    int queries_count = stoi(ltrim(rtrim(queries_count_temp)));
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queriesItem;
+        getline(cin, queriesItem);
+
+        queries[i] = queriesItem;
+    }
+
+    vector<int> res = matchingStrings(stringList, queries);
+
+    for (size_t i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
